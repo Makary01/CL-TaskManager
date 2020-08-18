b685e8f1f77c857468f2372a3747f6941ab7777b
Makary01 <makary.bortnowski@gmail.com>
2020-08-18 22:57:13 +0200

    działająca metoda loadTaskListFromFile

--- src/main/java/pl/coderslab/taskmanager/TaskManager.java
+++ src/main/java/pl/coderslab/taskmanager/TaskManager.java
@@ -1,7 +1,12 @@
 package pl.coderslab.taskmanager;
 
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.lang.invoke.SwitchPoint;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -11,21 +16,27 @@ public class TaskManager {
 
         TaskManager.run();
     }
+
     private static String[][] list = new String[0][];
 
     public static void run() {
 
         showWelcomeMessage();
-        loadTaskListFromFile();
-        while(true){
+        try {
+            loadTaskListFromFile();
+        } catch (IOException e) {
+            System.out.println("Błąd wczytywania pliku");
+            e.printStackTrace();
+        }
+        while (true) {
             showMainMenu();
             String userChoice = getUserChoice();
-            if(validateUserChoice(userChoice)){
+            if (validateUserChoice(userChoice)) {
                 executeValidChoice(userChoice);
-                if(isExitChoice(userChoice)){
+                if (isExitChoice(userChoice)) {
                     break;
                 }
-            }else{
+            } else {
                 executeInvalidChoice(userChoice);
             }
         }
@@ -37,13 +48,54 @@ public class TaskManager {
 
     }
 
-    private static void loadTaskListFromFile() {
+    private static void loadTaskListFromFile() throws IOException {
+        Path path = Paths.get("tasks.csv");
+        if(!Files.exists(path)){
+            System.out.println("File 'task.csv' not found.");
+        }
+        while (!Files.exists(path)) {
+            System.out.println("Create new 'task.csv' file? Y/N");
+            Scanner scanner = new Scanner(System.in);
+            switch (scanner.nextLine().toLowerCase().trim()) {
+                case "y":
+                    Files.createFile(path);
+                    System.out.println("Created new 'tasks.csv' file");
+                    break;
+                case "n":
+                    System.out.println("Create 'task.csv' file and confirm by pressing enter");
+                    scanner.nextLine();
+                    while (!Files.exists(path)) {
+                        System.out.println("Error, file not found.\nCreate 'task.csv' file and confirm by pressing enter");
+                        scanner.nextLine();
 
+                    }
+                default:
+                    System.out.println("Invalid answer, try again.");
+            }
+
+        }
+
+        try {
+            Scanner scanner = new Scanner(path);
+            int i = 0;
+            while (scanner.hasNextLine()) {
+                if (i >= list.length) {
+                    list = Arrays.copyOf(list, list.length + 1);
+                }
+                StringBuilder sb = new StringBuilder();
+                sb.append(scanner.nextLine());
+                list[i] = sb.toString().split(",");
+                i++;
+            }
+        } catch (IOException e) {
+            //
+        }
     }
 
+
     private static void executeValidChoice(String userChoice) {
-        switch (userChoice){
-            case"add":
+        switch (userChoice) {
+            case "add":
                 //kod dla add
                 //
                 break;
@@ -70,8 +122,9 @@ public class TaskManager {
     private static boolean validateUserChoice(String userChoice) {
         String[] validChoices = {"add", "list", "remove", "exit"};
         for (String validChoice : validChoices) {
-            if(userChoice.toLowerCase().trim().equals(validChoice)) return true;
-        }return false;
+            if (userChoice.toLowerCase().trim().equals(validChoice)) return true;
+        }
+        return false;
     }
 
     private static boolean isExitChoice(String userChoice) {
@@ -79,7 +132,7 @@ public class TaskManager {
     }
 
     private static void executeInvalidChoice(String userChoice) {
-        System.out.println(ConsoleColors.RED_BACKGROUND + "Invalid option "+ userChoice +" plz select valid option");
+        System.out.println(ConsoleColors.RED_BACKGROUND + "Invalid option '" + userChoice + "' plz select valid option");
     }
 
     private static void showMainMenu() {
